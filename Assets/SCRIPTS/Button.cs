e9a389b44af2bfe7f7aa7a7d1cb51696f115cdea
MONNET Thibaut <t.monnet@rubika-edu.com>
2017-05-15 17:14:24 +0200

    Mouse Base Done

---
+++ Assets/SCRIPTS/Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    public bool active;
+    private Renderer _rend;
+
+    void Start()
+    {
+        _rend = GetComponent<Renderer>();
+    }
+
+    public void Activate()
+    {
+        active = true;
+        _rend.material.color = Color.red;
+    }
+}
---
+++ Assets/SCRIPTS/Flag.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour {
+
+    public float timeToCaptureSolo = 5.0f;
+
+    private int players = 0;
+    private float _remainingTime;
+
+	void Start () {
+        _remainingTime = timeToCaptureSolo;
+	}
+	
+
+	void Update () {
+		if(players < 0)
+        {
+            _remainingTime -= Time.deltaTime * players;
+        }
+	}
+
+    //Trigger
+    public void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Mouse")
+        {
+            players++;
+        }
+    }
+
+    public void OnTriggerExit(Collider col)
+    {
+        if(col.tag == "Mouse")
+        {
+            players--;
+        }
+    }
+}
