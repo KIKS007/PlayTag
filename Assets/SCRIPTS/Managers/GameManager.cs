7e9974a4cd9869f4b7a74bf8bc63b04191b63911
MONNET Thibaut <t.monnet@rubika-edu.com>
2017-05-17 09:47:33 +0200

    directory changes

---
+++ Assets/SCRIPTS/Managers/GameManager.cs
@@ -0,0 +1,220 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Rewired;
+
+public enum GameState
+{
+    Playing,
+    Menu,
+    Victory,
+    Pause
+}
+
+public class GameManager : MonoBehaviour
+{
+	[Header ("Players")]
+	public int playersCount = 0;
+	public GameObject cat;
+	public List<GameObject> mouses = new List<GameObject> ();
+
+    [Header("Scores")]
+    public int catWinScore;
+    public int mousesWinScore;
+
+	[Header ("Prefabs")]
+	public GameObject catPrefab;
+	public GameObject mousePrefab;
+
+	[Header ("Spawns")]
+	public List<Transform> spawns = new List<Transform> ();
+
+    [Header("Button")]
+    public List<Interrupter> buttons;
+
+    [Header("Settings")]
+    public float timer;
+    public Text timerText;
+
+	public List<int> _controllerNumbers = new List<int> ();
+	private List<Transform> _spawnsTemp = new List<Transform> ();
+    public List<Mouse> _mouses = new List<Mouse>();
+    private List<int> _scoreList = new List<int>();
+    private float _timer;
+    
+    public static GameManager Instance;
+
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	void Start () 
+	{
+        Time.timeScale = 1f;
+		SpawnPlayers ();
+        _timer = timer;
+
+        foreach(GameObject go in mouses)
+        {
+            _mouses.Add(go.GetComponent<Mouse>());
+        }
+
+        for(int i = 0; i < playersCount; i++)
+        {
+            _scoreList.Add(0);
+        }
+    }
+
+	void Update () 
+	{
+        //  -TIMER-
+        if(_timer <= 0f)
+        {
+            //  -CAT VICTORY-
+            Victory(false);
+            timerText.text = "0.00";
+        }
+        else
+        {
+            _timer -= Time.deltaTime;
+            timerText.text = _timer.ToString("F2");
+        }
+        //
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Restart();
+        }
+	}
+
+	void SpawnPlayers ()
+	{
+		_controllerNumbers.Clear ();
+		mouses.Clear ();
+		_spawnsTemp.Clear ();
+		_spawnsTemp.AddRange (spawns);
+
+		if (ReInput.controllers.joystickCount == 0)
+		{
+			Debug.LogWarning ("No Joystick!");
+
+			playersCount = 2;
+
+			for(int i = 0; i < 2; i++)
+				_controllerNumbers.Add (i);
+		}
+		else
+		{
+			for(int i = 0; i < ReInput.controllers.joystickCount; i++)
+			{
+				if(i < 4)
+					_controllerNumbers.Add (i);
+			}
+
+			playersCount = ReInput.controllers.joystickCount;
+
+			if (playersCount > 4)
+				playersCount = 4;
+
+			if (playersCount == 1)
+			{
+				playersCount = 2;
+				_controllerNumbers.Add (1);
+			}
+		}
+
+		for(int i = 0; i < playersCount; i++)
+		{
+			int randomControllerNumber = _controllerNumbers [Random.Range (0, _controllerNumbers.Count)];
+			_controllerNumbers.Remove (randomControllerNumber);
+
+			if (i == 0)
+				SpawnCat (randomControllerNumber);
+			else
+				SpawnMouse (randomControllerNumber);
+		}
+	}
+
+	void SpawnCat (int controllerNumber)
+	{
+		Transform spawn = _spawnsTemp [Random.Range (0, _spawnsTemp.Count)];
+		_spawnsTemp.Remove (spawn);
+
+		cat = Instantiate (catPrefab, spawn.position, catPrefab.transform.rotation) as GameObject;
+		cat.GetComponent<Cat> ().SetupRewired (controllerNumber);
+	}
+
+	void SpawnMouse (int controllerNumber)
+	{
+		Transform spawn = _spawnsTemp [Random.Range (0, _spawnsTemp.Count)];
+		_spawnsTemp.Remove (spawn);
+
+		mouses.Add (Instantiate (mousePrefab, spawn.position, mousePrefab.transform.rotation) as GameObject);
+		mouses [mouses.Count - 1].GetComponent<Mouse> ().SetupRewired (controllerNumber);
+		mouses [mouses.Count - 1].GetComponent<Renderer> ().material.color = Random.ColorHSV ();
+	}
+
+    public void CheckButton()
+    {
+        foreach(Interrupter b in buttons)
+        {
+            if (!b.active)
+                return;
+        }
+
+        //  -MOUSE VICTORY-
+        Victory(true);
+    }
+
+    public void CheckMouse()
+    {
+        foreach(Mouse m in _mouses)
+        {
+            if(m.mouseState == MouseState.Normal)
+            {
+                return;
+            }
+        }
+
+        Victory(false);
+    }
+
+    void Victory(bool mouse)
+    {
+        Time.timeScale = 0f;
+
+        /*
+        if (mouse)
+        {
+            foreach (GameObject go in mouses)
+            {
+                _scoreList[go.GetComponent<Mouse>().controllerNumber] += mousesWinScore;
+            }
+        }
+        else
+        {
+            _scoreList[cat.GetComponent<Cat>().controllerNumber] += catWinScore;
+        }
+        for(int i = 0; i < _scoreList.Count; i++)
+        {
+            Debug.Log("j" + (i + 1) + " : " + _scoreList[i]);
+        }
+        */
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/SCRIPTS/Players/Mouse.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using DG.Tweening;
+
+public enum MouseState
+{
+    Normal,
+	Frozen
+}
+
+public enum PushState
+{
+	CanPush,
+	Pushing,
+	Cooldown
+}
+
+public class Mouse : MonoBehaviour 
+{
+	[Header("Controller Number")]
+	public int controllerNumber = -1;
+	[HideInInspector]
+	public Player rewiredPlayer; // The Rewired Player
+
+	[Header("States")]
+	public PushState pushState = PushState.CanPush;
+    public MouseState mouseState = MouseState.Normal;
+	public DashState dashState = DashState.CanDash;
+
+	[Header("Dash")]
+	public float dashSpeed = 80;
+	public float dashDuration;
+	public float dashCooldown = 1f;
+
+	[Header("Movement")]
+	public float speed = 16;
+
+	[Header("Push")]
+	public float pushDuration = 0.5f;
+	public float pushForce = 150f;
+	public float pushCooldown = 1f;
+    public GameObject pushAOE;
+
+	[Header("Wrap")]
+	public float xWidth;
+	public float yWidth;
+
+    private List<GameObject> _triggered = new List<GameObject>();
+	private Rigidbody _rigidbody;
+    private MeshRenderer _rend;
+    private Color _tempColor = new Color();
+	private float _dashSpeedTemp;
+
+	[HideInInspector]
+	public Vector3 _movement;
+
+	void Start () 
+	{
+		_rigidbody = GetComponent<Rigidbody> ();
+        _rend = GetComponent<MeshRenderer>();
+		rewiredPlayer = ReInput.players.GetPlayer(controllerNumber);
+	}
+
+	public void SetupRewired (int number)
+	{
+		controllerNumber = number;
+		rewiredPlayer = ReInput.players.GetPlayer(controllerNumber);
+	}
+	
+	void Update () 
+	{
+        if (mouseState == MouseState.Normal)
+        {
+            //Movement Vector
+            _movement = new Vector3(rewiredPlayer.GetAxisRaw("Move Horizontal"), 0f, rewiredPlayer.GetAxisRaw("Move Vertical"));
+            _movement.Normalize();
+
+            //Push
+            if (rewiredPlayer.GetButtonDown("Action 2") && pushState == PushState.CanPush)
+                Push();
+
+			//Dash
+			if (rewiredPlayer.GetButtonDown("Action 1") && dashState == DashState.CanDash)
+				StartCoroutine(Dash());
+
+            LookForward();
+        }
+	}
+
+	void FixedUpdate ()
+	{
+		Movement ();
+	}
+
+	void Movement ()
+	{
+		//Movement
+		_rigidbody.MovePosition(_rigidbody.position + _movement * speed * Time.fixedDeltaTime);
+	}
+
+	void LookForward ()
+	{
+		transform.LookAt (transform.position + _movement);
+	}
+
+
+	void Push ()
+	{
+		pushState = PushState.Pushing;
+        _triggered.Clear();
+        pushAOE.SetActive(true);
+
+
+		StartCoroutine(PushEnd());
+	}
+
+    //cooldown
+	protected virtual IEnumerator PushEnd ()
+	{
+		yield return new WaitForSeconds(pushDuration);
+		
+		pushAOE.SetActive(false);
+
+		pushState = PushState.Cooldown;
+
+		yield return new WaitForSeconds(pushCooldown);
+
+		pushState = PushState.CanPush;
+	}
+
+	protected virtual IEnumerator Dash()
+	{
+		dashState = DashState.Dashing;
+
+		_dashSpeedTemp = dashSpeed;
+
+		DOTween.To (()=> _dashSpeedTemp, x=> _dashSpeedTemp = x, 0, dashDuration).SetEase (Ease.OutQuad);
+
+		while (_dashSpeedTemp != 0)
+		{
+			_rigidbody.velocity = transform.forward * _dashSpeedTemp;
+
+			yield return new WaitForFixedUpdate();
+		}
+
+		dashState = DashState.Cooldown;
+
+		yield return new WaitForSeconds (dashCooldown);
+
+		dashState = DashState.CanDash;
+	}
+
+    //PushTrigger
+    void OnTriggerEnter (Collider col)
+    {
+        foreach(GameObject go in _triggered)
+        {
+            if (col.gameObject == go)
+                return;
+        }
+
+        _triggered.Add(col.gameObject);
+
+        //button
+        if (col.tag == "Button")
+        {
+            col.GetComponent<Interrupter>().Activate(); 
+        }
+
+        //movable
+        if (col.gameObject.layer == 8)
+        {
+            Rigidbody rb = col.GetComponent<Rigidbody>();
+            rb.AddForce((rb.position - transform.position).normalized * pushForce, ForceMode.Impulse);
+        }
+
+		//Cat
+		if (col.tag == "Cat")
+		{
+			if (col.GetComponent<Cat> ().catstate == CatState.Stunned)
+				return;
+			
+			Rigidbody rb = col.GetComponent<Rigidbody>();
+            rb.AddForce((rb.position - transform.position).normalized * pushForce, ForceMode.Impulse);
+            col.GetComponent<Cat>().StartCoroutine("Stun");
+        }
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Cat")
+        {
+			if (mouseState == MouseState.Normal && col.gameObject.GetComponent<Cat> ().catstate != CatState.Stunned)
+            {
+                mouseState = MouseState.Frozen;
+                _movement = Vector3.zero;
+
+                //frozen color change
+                _tempColor = _rend.material.color;
+                _rend.material.color = Color.blue;
+
+                GameManager.Instance.CheckMouse();
+            }
+        }
+
+        if (col.gameObject.tag == "Mouse")
+        {
+            if (mouseState == MouseState.Frozen)
+            {
+                _rend.material.color = _tempColor;
+
+                mouseState = MouseState.Normal;
+            }
+        }
+    }
+}
