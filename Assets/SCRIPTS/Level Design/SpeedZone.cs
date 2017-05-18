193adc8ba3dbb5342640b7a719928f0ece4494f0
MONNET Thibaut <t.monnet@rubika-edu.com>
2017-05-18 13:31:51 +0200

    Add cat/mouse handling for speedzone

--- Assets/SCRIPTS/Level Design/SpeedZone.cs
+++ Assets/SCRIPTS/Level Design/SpeedZone.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 
 public class SpeedZone : MonoBehaviour {
 
-    public float speedModifier;
+    public float catSpeedModifier;
+    public float mouseSpeedModifier;
 
     void OnTriggerEnter(Collider col)
     {
-        if(speedModifier != 0f && col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if(col.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            if (col.tag == "Mouse")
-                col.GetComponent<Mouse>().speedBoost = speedModifier;
-            else
-                col.GetComponent<Cat>().speedBoost = speedModifier;
+            if (mouseSpeedModifier != 0f && col.tag == "Mouse")
+            {
+                col.GetComponent<Mouse>().speedBoost = mouseSpeedModifier;
+            }
+            else if (catSpeedModifier != 0f)
+            {
+                col.GetComponent<Cat>().speedBoost = catSpeedModifier;
+            }
         }
     }
 
     void OnTriggerExit(Collider col)
     {
-        if (speedModifier != 0f && col.gameObject.layer == LayerMask.NameToLayer("Player"))
+        if (col.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            if (col.tag == "Mouse")
+            if (mouseSpeedModifier != 0f && col.tag == "Mouse")
+            {
                 col.GetComponent<Mouse>().speedBoost = 0f;
-            else
+            }
+            else if (catSpeedModifier != 0f)
+            {
                 col.GetComponent<Cat>().speedBoost = 0f;
+            }
         }
     }
 }
